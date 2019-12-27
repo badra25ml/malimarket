709e090e3bf266835a3efeb52cf48568cddeb2cb
Aliou S Maiga <badrasadja@gmail.com>
2019-12-27 23:11:27 +0000

    Added extra pages

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Page d'accueil" });
 });
 
+router.get('/apropos', function(req, res, next) {
+  res.render('propos', { title: 'A propos de nous' });
+});
 
+router.get('/register', function(req, res, next) {
+  res.render('index', { title: 'Registration page' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login Page' });
+});
 module.exports = router;
